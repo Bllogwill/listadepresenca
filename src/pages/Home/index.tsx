7c8659fde487001c1651934c9793609b068cf8b3
William Silva <tintilho@yahoo.com.br>
2022-07-16 11:25:35 +0100

    DIA160722

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,6 +6,22 @@ import {Card} from'../../componets/card';
 export function Home() {
   const [studentName, setStudentName] = useState<string>('');
   const [students, setStudents] = useState<any[]>([]);
+  const [user, setUser] = useState( {name:'', avatar:''});
+
+  useEffect(() => {
+    async function fetchData(){
+      const response = await fetch('https://api.github.com/users/bllogwill')
+      const data = await response.json();
+      console.log("DADOS ===>", data);
+
+      setUser({
+        name: data.name,
+        avatar: data.avatar_url,
+      });
+    } 
+        
+    fetchData();
+    }, []);
 
   function handleAddStudent(){
     const newStudent = {
@@ -18,17 +34,15 @@ export function Home() {
     }
     setStudents(prevState=>[...prevState, newStudent]);
   }
-    useEffect(() => {
-      console.log(" useEffect foi chamado!")
-    },[]);
+    
 
     return (
       <div className='container'>
       <header>
       <h1>Lista de Presenca</h1> 
       <div>
-        <strong>William Silva</strong>
-        <img src="https://viagemeturismo.abril.com.br/wp-content/uploads/2016/10/malta_-_valletta_from_marsamxett_harbour_01_by_clive_vella.jpeg?quality=70&strip=info&w=882&w=636" alt="Foto da malta" />
+        <strong>{user.name}</strong>
+        <img src={user.avatar} />
       </div>
         </header> 
 
